4abb7e2637112f92b56f2c7b7ce95fecf3e8917e
Muhammad Umair Khan <muhammadumair2511@gmail.com>
2024-10-20 17:46:21 +0500

    feat(app): added vercel config to host on vercel

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./database/index.js";
 import authRoutes from "./routes/routes.js";
 
 dotenv.config();
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "*",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
